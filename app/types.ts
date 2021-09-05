478e306e101234789eaf26c902e7b042bdd36eec
CREESTL <kroymw3@yandex.ru>
2021-09-05 22:07:31 +0300

    Delete Unused Types

--- app/types.ts
+++ app/types.ts
@@ -9,23 +9,3 @@ export interface TransactionReceiptWithEvents extends TransactionReceipt {
   events?: Array<Event>;
 }
 
-export interface ContractFactory<ContractType> {
-  attach(address: string): ContractType;
-  deploy(...args: any[]): ContractType;
-}
-
-export interface ContractData {
-  _format: string,
-  abi: Array<any>,
-  contractName: string,
-  bytecode: string,
-  sourceName: string,
-  deployedBytecode: string,
-  linkReferences: object,
-  deployedLinkReferences: object,
-}
-
-
-// TODO make an interface for configuration that is loaded from .yaml file
-// now this is done via .env
-export interface Config {}
