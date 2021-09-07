3d44ff5caf1505ad619e98f15ee8a0d1681a86f7
CREESTL <kroymw3@yandex.ru>
2021-09-07 19:59:09 +0300

    Rearrange Functions

--- testing/pending.ts
+++ testing/pending.ts
@@ -37,8 +37,11 @@ async function main(): Promise<void> {
 
 			  const {data} = transaction;
 
+
+			  // TODO add parsing for: pair mint (ERC20), factory.createPair
+
 			  // Function to parse "data" field from pending transaction
-			  async function parse_data(data: string){
+			  const parseAddLiquidityETHDataField = async (data: string) => {
 			  	const abiRouter = require('../artifacts/contracts/interfaces/IUniswapV2Router02.sol/IUniswapV2Router02.json').abi;
 			  	const uniswapRouter = new ethers.utils.Interface(abiRouter);
 			  	let parsed_data = uniswapRouter.decodeFunctionData("addLiquidityETH", data);
@@ -47,7 +50,7 @@ async function main(): Promise<void> {
 			  }
 
 			  if (data != "0x"){
-			  	console.log("Parsed data is ", await parse_data(data));
+			  	console.log("Parsed data is ", await parseAddLiquidityETHDataField(data));
 			  }
  
 			});
@@ -61,130 +64,129 @@ async function main(): Promise<void> {
 	};
 
 
+	// Function sends 1ETH from one wallet to another
+	async function sendSingleToken(): Promise <void>  {
+
+		console.log("\n\n\n");
+
+		// The provider to get info from local node
+		const provider = ethers.provider;
+
+		// Wallet == Signer
+		const wallets: SignerWithAddress[] = await ethers.getSigners();
+
+		const wallet0: SignerWithAddress = wallets[0];
+		const wallet1: SignerWithAddress = wallets[1];
+
+		// Simple description of transaction
+		const tx = {
+			to: wallet1.address,
+			value: ethers.utils.parseEther(SWAP_UNITS.toString()),
+		}
+
+
+		console.log("Sending transaction...");
+		const txResponse: TransactionResponse = await wallet0.sendTransaction(tx)
+		// Wait for transaction to be minted
+		const txReceipt: TransactionReceiptWithEvents = await txResponse.wait();
+		console.log("Sent transaction received, here is the receipt: \n", txReceipt);
+
+
+	}
+
+	// Function creates a pair of tokens and adds liquidity to it
+	async function createPairOfTokens(): Promise <void>{
+
+		console.log("\n\n\n");
+
+
+		// Function to locally deploy test tokens (TTokens)
+		const deployTToken = async (signer: Signer): Promise <string> => {
+			const TToken: ContractFactory = await ethers.getContractFactory("TToken");
+		  let tToken: Contract = await TToken.deploy("TToken", "TT", 18); // 1 TToken = 10^18 wei
+		  await tToken.deployed();
+		  console.log("TToken deployed successfully. Address:", tToken.address);
+
+		  return tToken.address;
+		}
+
+
+		const wallets: SignerWithAddress[] = await ethers.getSigners();
+		const wallet: SignerWithAddress = wallets[0];
+
+
+		// In order to create a *some token* / WETH pair on local fork of Uniswap we have to deploy that token first
+		const tTokenAddress: string = await deployTToken(wallet);
+
+
+		// Get tokens contracts using local ".sol" files and addresses of deployed tokens
+	  const uniswapRouter: Contract = await ethers.getContractAt("IUniswapV2Router02", ROUTER_ADDRESS);
+	  const uniswapFactory: Contract = await ethers.getContractAt("IUniswapV2Factory", await uniswapRouter.factory());
+	  const tToken: Contract = await ethers.getContractAt("TToken", tTokenAddress);
+	  const WETH: Contract = await ethers.getContractAt("IERC20", await uniswapRouter.WETH());
+
+
+	  console.log("WETH address is ", WETH.address);
+	  console.log("TToken address is ", tToken.address);
+	  console.log("Wallet address is ", wallet.address);
+	  console.log("Wallet ETH balance is ", ethers.utils.formatEther(await wallet.getBalance()));
+
+
+	  // Before adding tokens to the pool, we need to have some of them on the wallet - mint them
+	  console.log("Minting TTokens to the wallet...")
+	  const mintTTokenTx: TransactionResponse = await tToken.mint(wallet.address, ethers.utils.parseEther("1000"));
+	  const mintTTokenReceipt: TransactionReceiptWithEvents = await mintTTokenTx.wait();
+	  console.log("TToken minted successfully!");
+	  
+	  // And then we have to approve token transaction
+	 	console.log("Approving adding liquidity...");
+	  const approveTTokenTx: TransactionResponse = await tToken.approve(uniswapRouter.address, amountTokenDesired);
+	  await approveTTokenTx.wait();
+	  console.log("Approved!");
+
+	  console.log("\nTrying to add liquidity...");
+
+	  // Add some liquidity to the token in order for the pair not to have a zero address
+	  const txResponse: TransactionResponse = await uniswapRouter.addLiquidityETH(
+	  		// All following amounts are measured in wei (not ETH!!!)
+	  		// The token that receives that liquidity
+	  		tToken.address,
+	  		// The amount of tokens to add to the pool if there is less tokens than WETH in the pool
+	  		amountTokenDesired,
+	  		// If WETH/token price goes up to 1/1 ratio - the transaction reverts
+	  		ethers.utils.parseEther("1"),
+	  		ethers.utils.parseEther("1"),
+	  		// Recipient of the liquidity tokens
+	  		wallet.address,
+	  		// Deadline after which the transaction reverts
+	  		Date.now() + 1000 * 60 * 10,
+	  		// the amount of WETH to add to the pool if there is less WETH thatn tokens in the pool
+	  		{value: amountETHDesired}, // Single ETH
+	  	)
+
+
+	  const txReceipt: TransactionReceiptWithEvents = await txResponse.wait();
+
+	  console.log("Liquidity added!");
+
+	  // Only after adding liquidity pair has non-zerro(0x0000000.... address)
+	  const pairAddress: string = await uniswapFactory.getPair(WETH.address, tToken.address);
+	  const uniswapPair: Contract = await ethers.getContractAt("IUniswapV2Pair", pairAddress);
+
+	  console.log("Address of the pair:", pairAddress);
+
+
+
+
+	  console.log("\n\n\n");
+
+		}
+
+
 	async function createPendingTransactions(): Promise <void> {
-		
-		// Function sends 1ETH from one wallet to another
-		async function sendSingleToken(): Promise <void>  {
-
-			console.log("\n\n\n");
-
-			// The provider to get info from local node
-			const provider = ethers.provider;
-
-			// Wallet == Signer
-			const wallets: SignerWithAddress[] = await ethers.getSigners();
-
-			const wallet0: SignerWithAddress = wallets[0];
-			const wallet1: SignerWithAddress = wallets[1];
-
-			// Simple description of transaction
-			const tx = {
-				to: wallet1.address,
-				value: ethers.utils.parseEther(SWAP_UNITS.toString()),
-			}
-
-
-			console.log("Sending transaction...");
-			const txResponse: TransactionResponse = await wallet0.sendTransaction(tx)
-			// Wait for transaction to be minted
-			const txReceipt: TransactionReceiptWithEvents = await txResponse.wait();
-			console.log("Sent transaction received, here is the receipt: \n", txReceipt);
-
-
-		}
-
-		// Function creates a pair of tokens and adds liquidity to it
-		async function createPairOfTokens(): Promise <void>{
-
-			console.log("\n\n\n");
-
-
-			// Function to locally deploy test tokens (TTokens)
-			async function deployTToken(signer: Signer): Promise <string> {
-				const TToken: ContractFactory = await ethers.getContractFactory("TToken");
-			  let tToken: Contract = await TToken.deploy("TToken", "TT", 18); // 1 TToken = 10^18 wei
-			  await tToken.deployed();
-			  console.log("TToken deployed successfully. Address:", tToken.address);
-
-			  return tToken.address;
-			}
-
-
-			const wallets: SignerWithAddress[] = await ethers.getSigners();
-			const wallet: SignerWithAddress = wallets[0];
-
-
-			// In order to create a *some token* / WETH pair on local fork of Uniswap we have to deploy that token first
-			const tTokenAddress: string = await deployTToken(wallet);
-
-
-			// Get tokens contracts using local ".sol" files and addresses of deployed tokens
-		  const uniswapRouter: Contract = await ethers.getContractAt("IUniswapV2Router02", ROUTER_ADDRESS);
-		  const uniswapFactory: Contract = await ethers.getContractAt("IUniswapV2Factory", await uniswapRouter.factory());
-		  const tToken: Contract = await ethers.getContractAt("TToken", tTokenAddress);
-		  const WETH: Contract = await ethers.getContractAt("IERC20", await uniswapRouter.WETH());
-
-
-		  console.log("WETH address is ", WETH.address);
-		  console.log("TToken address is ", tToken.address);
-		  console.log("Wallet address is ", wallet.address);
-		  console.log("Wallet ETH balance is ", ethers.utils.formatEther(await wallet.getBalance()));
-
-
-		  // Before adding tokens to the pool, we need to have some of them on the wallet - mint them
-		  console.log("Minting TTokens to the wallet...")
-		  const mintTTokenTx: TransactionResponse = await tToken.mint(wallet.address, ethers.utils.parseEther("1000"));
-		  const mintTTokenReceipt: TransactionReceiptWithEvents = await mintTTokenTx.wait();
-		  console.log("TToken minted successfully!");
-		  
-		  // And then we have to approve token transaction
-		 	console.log("Approving adding liquidity...");
-		  const approveTTokenTx: TransactionResponse = await tToken.approve(uniswapRouter.address, amountTokenDesired);
-		  await approveTTokenTx.wait();
-		  console.log("Approved!");
-
-		  console.log("\nTrying to add liquidity...");
-
-		  // Add some liquidity to the token in order for the pair not to have a zero address
-		  const txResponse: TransactionResponse = await uniswapRouter.addLiquidityETH(
-		  		// All following amounts are measured in wei (not ETH!!!)
-		  		// The token that receives that liquidity
-		  		tToken.address,
-		  		// The amount of tokens to add to the pool if there is less tokens than WETH in the pool
-		  		amountTokenDesired,
-		  		// If WETH/token price goes up to 1/1 ratio - the transaction reverts
-		  		ethers.utils.parseEther("1"),
-		  		ethers.utils.parseEther("1"),
-		  		// Recipient of the liquidity tokens
-		  		wallet.address,
-		  		// Deadline after which the transaction reverts
-		  		Date.now() + 1000 * 60 * 10,
-		  		// the amount of WETH to add to the pool if there is less WETH thatn tokens in the pool
-		  		{value: amountETHDesired}, // Single ETH
-		  	)
-
-
-		  const txReceipt: TransactionReceiptWithEvents = await txResponse.wait();
-
-		  console.log("Liquidity added!");
-
-		  // Only after adding liquidity pair has non-zerro(0x0000000.... address)
-		  const pairAddress: string = await uniswapFactory.getPair(WETH.address, tToken.address);
-		  const uniswapPair: Contract = await ethers.getContractAt("IUniswapV2Pair", pairAddress);
-
-		  console.log("Address of the pair:", pairAddress);
-
-
-
-
-		  console.log("\n\n\n");
-
-		}
-
 
 		await sendSingleToken();
 		await createPairOfTokens();
-
 
 	}
 
